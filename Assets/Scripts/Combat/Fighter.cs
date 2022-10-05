51c5620e07b45630d92a1935a8c8c900b2dffe45
Michael Stables <michael.stables1991@gmail.com>
2022-10-05 16:35:48 -0500

    Set up Combat Module
    Added Action Scheduler and created fighter script to move a character within combat range and activate an "attack" function

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+
+        private void Update()
+        {
+            if (target == null) return;
+            if (!IsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+            }
+        }
+
+        private bool IsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget target)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            Debug.Log("Take that you short, squat peasant!");
+            this.target = target.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null; 
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.ResetPath();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("Forward Speed", speed);
+        }
+
+        // Animation Event During Run Forward Animation
+        public void FootR() { }
+
+        // Animation Event During Run Forward Animation
+        public void FootL() { }
+    }
+}
+
